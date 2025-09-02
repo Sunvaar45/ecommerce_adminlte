4917dd40cbc81f46727271c417b66d7ac35c0a7f
Sunvaar45 <is.kalay1@gmail.com>
2025-09-02 10:05:00 +0300

    Implement product description editing functionality with dedicated Blade view

--- app/Http/Controllers/ProductDescriptionController.php
+++ app/Http/Controllers/ProductDescriptionController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductDescriptionController extends Controller
 {
-    public function edit()
+    public function edit($productId)
     {
+        $product = Products::findOrFail($productId);
 
+        return view('products-descriptions-edit', [
+            'product' => $product,
+        ]);
     }
 
     public function update(Request $request, $id)
---
+++ resources/views/products-descriptions-edit.blade.php
@@ -0,0 +1,40 @@
+@extends('adminlte::page')
+
+@section('title', 'Ürün Düzenle')
+
+@section('content_header')
+<h1>"{{ $product->name }}" Ürünü Açıklaması</h1>
+@stop
+
+@section('content')
+
+<x-success-alert />
+
+<form method="POST" action="{{ route('products-description.update', $product->id) }}">
+    @csrf
+    <div>
+        <textarea id="content-editor" name="content">{{ $product->description }}</textarea>
+    </div>
+
+    <x-update-buttons />
+</form>
+
+@stop
+
+@section('css')
+{{-- Add here extra stylesheets --}}
+{{--
+<link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+<script src="https://cdn.ckeditor.com/ckeditor5/35.4.0/classic/ckeditor.js"></script>
+
+<script>
+    ClassicEditor
+        .create(document.querySelector('#content-editor'))
+        .catch(error => {
+            console.error(error);
+        });
+</script>
+@stop
