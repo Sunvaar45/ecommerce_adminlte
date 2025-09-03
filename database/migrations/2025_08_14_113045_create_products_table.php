7dc98f34c214040362bdfa54d2ea16642e06fd33
Sunvaar45 <is.kalay1@gmail.com>
2025-09-03 09:53:58 +0300

    Create migration files for categories, products, and product_images tables with necessary fields and relationships

---
+++ database/migrations/2025_08_14_113045_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->boolean('has_discount')->default(false);
+            $table->decimal('discount_price', 10, 2)->nullable();
+            $table->integer('stock')->default(0);
+            $table->string('color')->nullable();
+            $table->string('image_url')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
