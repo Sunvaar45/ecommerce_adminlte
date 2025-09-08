3baeb59feabcf96b2b6f7cc1e9beb08995110779
Sunvaar45 <is.kalay1@gmail.com>
2025-09-08 17:34:46 +0300

    Implement admin authentication middleware to restrict access to admin routes

--- routes/web.php
+++ routes/web.php
@@ -21,55 +21,58 @@ Route::redirect('/home', '/admin/home');
 Route::prefix('admin/')->group(function () {
     Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
     Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.post');
-    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
 });
 
-// Home Route
-Route::get('/admin/home', [HomeController::class, 'index'])->name('home');
+Route::middleware('auth:admin')->group(function () {
+    Route::post('admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
 
-// Admin Actions
-Route::prefix('admin-actions')->group(function () {
-    Route::get('/set-active-state/{table}/{id}', [AdminActionsController::class, 'setActiveState'])->name('set-active-state');
-    Route::get('/delete/{table}/{id}', [AdminActionsController::class, 'delete'])->name('delete');
-});
+    // Home Route
+    Route::get('/admin/home', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('/admin/favicon-and-title')->group(function () {
-    Route::get('/', [FaviconAndTitleController::class, 'edit'])->name('favicon-and-title.edit');
-    Route::post('/update', [FaviconAndTitleController::class, 'update'])->name('favicon-and-title.update');
-});
+    // Admin Actions
+    Route::prefix('admin-actions')->group(function () {
+        Route::get('/set-active-state/{table}/{id}', [AdminActionsController::class, 'setActiveState'])->name('set-active-state');
+        Route::get('/delete/{table}/{id}', [AdminActionsController::class, 'delete'])->name('delete');
+    });
 
-// categories
-Route::prefix('/admin/categories')->group(function () {
-    Route::get('/', [CategoriesController::class, 'edit'])->name('categories.edit');
-    Route::post('/update', [CategoriesController::class, 'update'])->name('categories.update');
-});
+    Route::prefix('/admin/favicon-and-title')->group(function () {
+        Route::get('/', [FaviconAndTitleController::class, 'edit'])->name('favicon-and-title.edit');
+        Route::post('/update', [FaviconAndTitleController::class, 'update'])->name('favicon-and-title.update');
+    });
 
-// attributes
-Route::prefix('admin/attributes')->group(function () {
-    Route::get('/', [AttributesController::class, 'edit'])->name('attributes.edit');
-    Route::post('/update', [AttributesController::class, 'update'])->name('attributes.update');
-});
+    // categories
+    Route::prefix('/admin/categories')->group(function () {
+        Route::get('/', [CategoriesController::class, 'edit'])->name('categories.edit');
+        Route::post('/update', [CategoriesController::class, 'update'])->name('categories.update');
+    });
 
-Route::prefix('/admin/products')->group(function () {
-    // product
-    Route::get('/', [ProductsController::class, 'edit'])->name('products.edit');
-    Route::post('/update', [ProductsController::class, 'update'])->name('products.update');
+    // attributes
+    Route::prefix('admin/attributes')->group(function () {
+        Route::get('/', [AttributesController::class, 'edit'])->name('attributes.edit');
+        Route::post('/update', [AttributesController::class, 'update'])->name('attributes.update');
+    });
 
-    // product description
-    Route::get('/description/{id}', [ProductDescriptionController::class, 'edit'])->name('products-description.edit');
-    Route::post('/description/{id}/update', [ProductDescriptionController::class, 'update'])->name('products-description.update');
+    Route::prefix('/admin/products')->group(function () {
+        // product
+        Route::get('/', [ProductsController::class, 'edit'])->name('products.edit');
+        Route::post('/update', [ProductsController::class, 'update'])->name('products.update');
 
-    // product images
-    Route::get('/images', [ProductImagesController::class, 'edit'])->name('product-images.edit');
-    Route::post('/images/update', [ProductImagesController::class, 'update'])->name('product-images.update');
-    Route::get('/images/set-main/{id}', [ProductImagesController::class, 'setMainImage'])->name('product-images.set-main');
+        // product description
+        Route::get('/description/{id}', [ProductDescriptionController::class, 'edit'])->name('products-description.edit');
+        Route::post('/description/{id}/update', [ProductDescriptionController::class, 'update'])->name('products-description.update');
 
-    // product attribute values
-    Route::get('/attribute-values', [ProductAttributeValuesController::class, 'edit'])->name('product-attribute-values.edit');
-    Route::post('/attribute-values/update', [ProductAttributeValuesController::class, 'update'])->name('product-attribute-values.update');
-});
+        // product images
+        Route::get('/images', [ProductImagesController::class, 'edit'])->name('product-images.edit');
+        Route::post('/images/update', [ProductImagesController::class, 'update'])->name('product-images.update');
+        Route::get('/images/set-main/{id}', [ProductImagesController::class, 'setMainImage'])->name('product-images.set-main');
 
-Route::prefix('admin/')->group(function () {
-    Route::get('/account-information', [AccountController::class, 'edit'])->name('account.info.edit');
-    Route::post('/account-information/update', [AccountController::class, 'update'])->name('account.info.update');
+        // product attribute values
+        Route::get('/attribute-values', [ProductAttributeValuesController::class, 'edit'])->name('product-attribute-values.edit');
+        Route::post('/attribute-values/update', [ProductAttributeValuesController::class, 'update'])->name('product-attribute-values.update');
+    });
+
+    Route::prefix('admin/')->group(function () {
+        Route::get('/account-information', [AccountController::class, 'edit'])->name('account.info.edit');
+        Route::post('/account-information/update', [AccountController::class, 'update'])->name('account.info.update');
+    });
 });
